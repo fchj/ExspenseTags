d635aa79b29378a96e7111e29bbd7216430a9765
Felix Carsten Heyn-Johnsen <felix.heyn-johnsen@autohuset-vestergaard.dk>
2024-06-30 14:00:14 +0200

    adds dto

---
+++ TransActionLineDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExspenseTags
+{
+	public class TransActionLineDto
+	{
+		public DateOnly TransactionDate {  get; set; }
+		public string BankCategory { get; set; }
+
+		public string BankSubCategory { get; set; }
+
+		public string Body { get; set; }
+
+		public decimal Amount { get; set; }
+
+		public decimal Balance { get; set; }
+
+	}
+}
